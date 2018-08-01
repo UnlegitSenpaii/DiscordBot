f4b80ef543c2455977598e5f9151b41d17c3e1c1
Senpaii <dashaterdas@gmail.com>
2018-08-01 09:07:41 +0200

    added code

---
+++ main.js
@@ -0,0 +1,49 @@
+const cmd = require("discord.js-commando");
+const bot = new cmd.Client();
+const discord = require("discord.js");
+
+bot.registry.registerGroup("musik", "Musik");
+bot.registry.registerGroup("simple", "Simple");
+
+bot.registerCommandsIn(__dirname + "/commands");
+
+
+bot.on("message", function(message){
+    //chatfilter
+        //liste der bösen wörtern   (immer klein schreiben!)
+    let blacklisted = [
+        "nazi",
+        "schlampe",
+        "wichser",
+        "wixxer",
+        "wixer",
+        ""
+    ];
+    var foundbadword = false;
+
+    for(var i in blacklisted){//nach bösen wörtern suchen c;
+        if(message.content.toLowerCase().includes(blacklisted[i].toLowerCase()))
+        foundbadword = true;
+    }
+    if (foundbadword)
+    {
+        message.delete();//löschen von dem bösen wort c;
+        message.author.send(`Bitte achte auf deine Wortwahl!`);
+    }
+
+
+});
+
+
+bot.on("ready", function(){
+
+
+});
+
+
+
+
+
+
+4
+bot.login(process.env.BOT_TOKEN);   //immer auf der letzten zeile!
