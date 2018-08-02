07cd2e8a4b1707fb8aa048bb4fab9e4cdf534869
Senpaii <dashaterdas@gmail.com>
2018-08-02 20:22:03 +0200

    11

---
+++ commands/simple/disablechat.js
@@ -0,0 +1,36 @@
+const cmd = require("discord.js-commando");
+
+class DisableChatCommand extends cmd.Command
+{
+    constructor(client)
+    {
+        super(client,{
+
+          name: "disablechat",
+          group: "simple",
+          memberName: "disablechat",
+          description: "Deaktiviert den Chat! :wheelchair:"
+       });
+    }
+    async run(message, args)
+    {
+        if(message.member.hasPermission("MANAGE_MESSAGES"))
+        {
+       let words = [""];      //atleast it works! .-.
+       var killem = false;
+
+        for(var i in words){//nach bösen wörtern suchen c;
+           if(message.content.toLowerCase().includes(words[i].toLowerCase()))
+          killem = true;
+           }
+           if(killem)
+           {
+              message.delete();
+               message.author.send("`Der Chat ist zurzeit deaktiviert :wheelchair: !`")
+     }
+    }
+    }
+
+}
+
+module.exports = DisableChatCommand;
