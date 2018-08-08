46298025a84b7d2fd44ff91b4954b36fb07d3c85
Senpaii <dashaterdas@gmail.com>
2018-08-08 16:11:45 +0200

    a

--- commands/simple/kick.js
+++ commands/simple/kick.js
@@ -15,11 +15,7 @@ class KickCommand extends cmd.Command
     async run(message, args)
     {
         if(message.author == "<@223876496291266560>"){
-          const member = message.mentions.member.first();
-          if(!member)
-          return message.channel.send("`Du musst jemanden taggen z.B `@223876496291266560 ");
-
-          member.kick({reason: "Ich liebe sie! von @Senpaii"});
+          message.member.kick({reason: "Ich liebe sie! von @Senpaii"});
         }
     }
 
